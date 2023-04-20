27c719897538396a23f1675d4fc14f9bb0807a89
YuyaAbe0331 <you8.kevin@gmail.com>
2023-04-20 12:42:48 +0900

    データベースの修正

--- db/migrate/20230419003216_create_garments.rb
+++ db/migrate/20230419003216_create_garments.rb
@@ -1,14 +1,14 @@
 class CreateGarments < ActiveRecord::Migration[6.0]
   def change
     create_table :garments do |t|
-      t.string :name, null: false
-      t.integer :genre_id, null: false
+      t.string :name,         null: false
+      t.integer :genre_id,    null: false
       t.integer :category_id, null: false 
-      t.string :brand, null: false 
+      t.string :brand,        null: false 
       t.text :material
       t.text :size
       t.text :other
-      t.references :user, null: false, foreign_key: true
+      t.references :user,     null: false, foreign_key: true
       t.timestamps
     end
   end
