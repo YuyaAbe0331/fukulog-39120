bf2b962d7cd953d8a2c1c4c89aae618aae43951c
YuyaAbe0331 <you8.kevin@gmail.com>
2023-05-12 21:15:35 +0900

    ユーザー新規登録の結合テストコードの作成

---
+++ spec/system/users_spec.rb
@@ -0,0 +1,64 @@
+require 'rails_helper'
+
+def basic_pass(path)
+  username = ENV["BASIC_AUTH_USER"]
+  password = ENV["BASIC_AUTH_PASSWORD"]
+  visit "http://#{username}:#{password}@#{Capybara.current_session.server.host}:#{Capybara.current_session.server.port}#{path}"
+end
+
+RSpec.describe "ユーザー新規登録", type: :system do
+  before do
+    @user = FactoryBot.build(:user)
+  end
+  context 'ユーザー新規登録ができるとき' do 
+    it '正しい情報を入力すればユーザー新規登録ができてトップページに移動する' do
+      # トップページに移動する
+      basic_pass root_path
+      visit root_path
+      # トップページにサインアップページへ遷移するボタンがあることを確認する
+      expect(page).to have_content('新規登録')
+      # 新規登録ページへ移動する
+      visit new_user_registration_path
+      # ユーザー情報を入力する
+      fill_in 'メールアドレス', with: @user.email
+      fill_in 'パスワード', with: @user.password
+      fill_in 'パスワード（確認）', with: @user.password_confirmation
+      fill_in 'ニックネーム', with: @user.nickname
+      select "男性", from: 'user[sex_id]'
+      select "161 - 165cm", from: 'user[height_id]'
+      select "71 - 75kg", from: 'user[weight_id]'
+      # サインアップボタンを押すとユーザーモデルのカウントが1上がることを確認する
+      expect{
+        find('input.btn-primary').click
+      }.to change { User.count }.by(1)
+      # トップページへ遷移したことを確認する
+      expect(current_path).to eq(root_path)
+      # ヘッダーにログアウトボタンが表示されることを確認する
+      expect(page).to have_content('ログアウト')
+      # ヘッダーにユーザ名が表示されていることを確認する
+      expect(page).to have_content(@user.nickname)
+    end
+  end
+  context 'ユーザー新規登録ができないとき' do
+    it '誤った情報ではユーザー新規登録ができずに新規登録ページへ戻ってくる' do
+      # トップページに移動する
+      visit root_path
+      # トップページにサインアップページへ遷移するボタンがあることを確認する
+      expect(page).to have_content('新規登録')
+      # 新規登録ページへ移動する
+      visit new_user_registration_path
+      # ユーザー情報を入力する
+      fill_in 'メールアドレス', with: ''
+      fill_in 'パスワード', with: ''
+      fill_in 'パスワード（確認）', with: ''
+      fill_in 'ニックネーム', with: ''
+      select "---", from: 'user[sex_id]'
+      # サインアップボタンを押してもユーザーモデルのカウントは上がらないことを確認する
+      expect{
+        find('input.btn-primary').click
+      }.to change { User.count }.by(0)
+      # 新規登録ページへ戻されることを確認する
+      expect(current_path).to eq user_registration_path
+    end
+  end
+end
