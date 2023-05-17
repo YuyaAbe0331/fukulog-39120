cda250c3e4f502a0cb4a04c785408054ce6f29ae
YuyaAbe0331 <you8.kevin@gmail.com>
2023-05-18 00:13:31 +0900

    showアクションのコントローラーのテストの作成

--- spec/requests/garments_spec.rb
+++ spec/requests/garments_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
+  def basic_index_pass(pass)
+    username = ENV["BASIC_AUTH_USER"]
+    password = ENV["BASIC_AUTH_PASSWORD"]
+    get root_path, headers: { 'HTTP_AUTHORIZATION' => ActionController::HttpAuthentication::Basic.encode_credentials(username, password) }
+  end
+
+  def basic_show_pass(pass)
+    username = ENV["BASIC_AUTH_USER"]
+    password = ENV["BASIC_AUTH_PASSWORD"]
+    get garment_path(@garment), headers: { 'HTTP_AUTHORIZATION' => ActionController::HttpAuthentication::Basic.encode_credentials(username, password) }
+  end
+
 RSpec.describe "Garments", type: :request do
   
   before do
@@ -8,46 +20,75 @@ RSpec.describe "Garments", type: :request do
 
   describe "GET /garments" do
     it 'indexアクションにリクエストすると正常にレスポンスが返ってくる' do
-      username = ENV["BASIC_AUTH_USER"]
-      password = ENV["BASIC_AUTH_PASSWORD"]
-      get root_path, headers: { 'HTTP_AUTHORIZATION' => ActionController::HttpAuthentication::Basic.encode_credentials(username, password) }
+      basic_index_pass root_path
       expect(response.status).to eq 200
     end
     it 'indexアクションにリクエストするとレスポンスに投稿済みの洋服の画像が存在する' do 
-      username = ENV["BASIC_AUTH_USER"]
-      password = ENV["BASIC_AUTH_PASSWORD"]
-      get root_path, headers: { 'HTTP_AUTHORIZATION' => ActionController::HttpAuthentication::Basic.encode_credentials(username, password) }
+      basic_index_pass root_path
       expect(response.body).to include('test_image1.jpeg')
     end
     it 'indexアクションにリクエストするとレスポンスに投稿済みの洋服のnameが存在する' do 
-      username = ENV["BASIC_AUTH_USER"]
-      password = ENV["BASIC_AUTH_PASSWORD"]
-      get root_path, headers: { 'HTTP_AUTHORIZATION' => ActionController::HttpAuthentication::Basic.encode_credentials(username, password) }
+      basic_index_pass root_path
       expect(response.body).to include(@garment.name)
     end
     it 'indexアクションにリクエストするとレスポンスに投稿済みの洋服のcategoryが存在する' do
-      username = ENV["BASIC_AUTH_USER"]
-      password = ENV["BASIC_AUTH_PASSWORD"]
-      get root_path, headers: { 'HTTP_AUTHORIZATION' => ActionController::HttpAuthentication::Basic.encode_credentials(username, password) }
+      basic_index_pass root_path
       expect(response.body).to include(@garment.category.name)
     end
     it 'indexアクションにリクエストするとレスポンスに投稿済みの洋服のbrandが存在する' do
-      username = ENV["BASIC_AUTH_USER"]
-      password = ENV["BASIC_AUTH_PASSWORD"]
-      get root_path, headers: { 'HTTP_AUTHORIZATION' => ActionController::HttpAuthentication::Basic.encode_credentials(username, password) }
+      basic_index_pass root_path
       expect(response.body).to include(@garment.brand)
     end
     it 'indexアクションにリクエストするとレスポンスに投稿済みの洋服の投稿者名が存在する' do
-      username = ENV["BASIC_AUTH_USER"]
-      password = ENV["BASIC_AUTH_PASSWORD"]
-      get root_path, headers: { 'HTTP_AUTHORIZATION' => ActionController::HttpAuthentication::Basic.encode_credentials(username, password) }
+      basic_index_pass root_path
       expect(response.body).to include(@garment.user.nickname)
     end
     it 'indexアクションにリクエストするとレスポンスに投稿検索フォームが存在する' do
-      username = ENV["BASIC_AUTH_USER"]
-      password = ENV["BASIC_AUTH_PASSWORD"]
-      get root_path, headers: { 'HTTP_AUTHORIZATION' => ActionController::HttpAuthentication::Basic.encode_credentials(username, password) }
+      basic_index_pass root_path
       expect(response.body).to include('Search')
     end
   end
+
+  describe 'GET #show' do
+    it 'showアクションにリクエストすると正常にレスポンスが返ってくる' do
+      basic_show_pass garment_path(@garment)
+      expect(response.status).to eq 200
+    end
+    it 'showアクションにリクエストするとレスポンスに投稿済みの洋服の画像が存在する' do 
+      basic_show_pass garment_path(@garment)
+      expect(response.body).to include('test_image1.jpeg')
+    end
+    it 'showアクションにリクエストするとレスポンスに投稿済みの洋服のnameが存在する' do
+      basic_show_pass garment_path(@garment)
+      expect(response.body).to include(@garment.name)
+    end
+    it 'showアクションにリクエストするとレスポンスに投稿済みの洋服のgenreが存在する' do 
+      basic_show_pass garment_path(@garment)
+      expect(response.body).to include(@garment.genre.name)
+    end
+    it 'showアクションにリクエストするとレスポンスに投稿済みの洋服のcategoryが存在する' do 
+      basic_show_pass garment_path(@garment)
+      expect(response.body).to include(@garment.category.name)
+    end
+    it 'showアクションにリクエストするとレスポンスに投稿済みの洋服のbrandが存在する' do 
+      basic_show_pass garment_path(@garment)
+      expect(response.body).to include(@garment.brand)
+    end
+    it 'showアクションにリクエストするとレスポンスに素材の項目が存在する' do 
+      basic_show_pass garment_path(@garment)
+      expect(response.body).to include('素材')
+    end
+    it 'showアクションにリクエストするとレスポンスにサイズの項目が存在する' do 
+      basic_show_pass garment_path(@garment)
+      expect(response.body).to include('サイズ')
+    end
+    it 'showアクションにリクエストするとレスポンスにその他の項目が存在する' do 
+      basic_show_pass garment_path(@garment)
+      expect(response.body).to include('その他')
+    end
+    it 'showアクションにリクエストするとレスポンスに投稿者名が存在する' do 
+      basic_show_pass garment_path(@garment)
+      expect(response.body).to include(@garment.user.nickname)
+    end
+  end 
 end
