a3abdc5b4651ee3e2350969eaa92039f8a6fbdd0
YuyaAbe0331 <you8.kevin@gmail.com>
2023-06-01 17:35:43 +0900

    ログインしたユーザーは新規投稿できる場合の結合テストコードの作成

--- spec/system/garments_spec.rb
+++ spec/system/garments_spec.rb
@@ -10,6 +10,11 @@ end
 RSpec.describe "Garments", type: :system do
   before do
     @user = FactoryBot.create(:user)
+    @garment_name = Faker::Lorem.sentence
+    @garment_brand = Faker::Lorem.sentence
+    @garment_material = Faker::Lorem.paragraph
+    @garment_size = Faker::Lorem.paragraph
+    @garment_other = Faker::Lorem.paragraph
   end
   context '洋服投稿ができるとき'do
     it 'ログインしたユーザーは新規投稿できる' do
@@ -28,16 +33,33 @@ RSpec.describe "Garments", type: :system do
       image_path = Rails.root.join('public/images/test_image1.jpeg')
       # フォームに情報を入力する
       attach_file('garment[image]', image_path, make_visible: true)
-      fill_in '洋服名', with: Faker::Lorem.sentence
+      fill_in '洋服名', with: @garment_name
+      select 'レディース', from: 'garment[genre_id]'
+      select 'Tシャツ', from: 'garment[category_id]'
+      fill_in 'ブランド', with: @garment_brand
+      fill_in '素材', with: @garment_material
+      fill_in 'サイズ', with: @garment_size
+      fill_in 'その他', with: @garment_other
       # 送信するとGarmentsモデルのカウントが1上がることを確認する
+      expect{
+        find('input.btn-primary').click
+      }.to change { Garment.count }.by(1)
       # 投稿完了ページに遷移することを確認する
-      # 「投稿が完了しました」の文字があることを確認する
+      expect(current_path).to eq(garments_path)
+      # 「登録が完了しました！」の文字があることを確認する
+      expect(page).to have_content('登録が完了しました！')
       # トップページに遷移する
+      visit root_path
       # トップページには先ほど投稿した内容の洋服が存在することを確認する（image）
+      expect(page).to have_selector("img[src$='test_image1.jpeg']")
       # トップページには先ほど投稿した内容の洋服が存在することを確認する（name）
+      expect(page).to have_content(@garment_name)
       # トップページには先ほど投稿した内容の洋服が存在することを確認する（category.name）
+      expect(page).to have_content('Tシャツ')
       # トップページには先ほど投稿した内容の洋服が存在することを確認する（brand）
-      # トップページには先ほど投稿したユーザ名が表示されることを確認する（@user.name）
+      expect(page).to have_content(@garment_brand)
+      # トップページには先ほど投稿したユーザ名が表示されることを確認する（@user.nickname）
+      expect(page).to have_content(@user.nickname)
     end
   end
   context '洋服投稿ができないとき'do
