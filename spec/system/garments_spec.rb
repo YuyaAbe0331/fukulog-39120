392505bff25ccd0c94a58c6612070bfc7e29b8ce
YuyaAbe0331 <you8.kevin@gmail.com>
2023-06-24 22:54:45 +0900

    素材情報編集の結合テストの記述

--- spec/system/garments_spec.rb
+++ spec/system/garments_spec.rb
@@ -124,6 +124,7 @@ RSpec.describe '投稿内容の編集', type: :system do
       select 'キッズ', from: 'garment[genre_id]'
       select 'その他', from: 'garment[category_id]'
       fill_in 'garment_brand', with: Faker::Lorem.sentence
+      fill_in 'garment_material', with: Faker::Lorem.paragraph 
       # 編集してもGarmentモデルのカウントは変わらないことを確認する
       # 編集完了後、詳細ページに遷移する
       # 先ほど変更した内容のツイートが存在することを確認する（image）
