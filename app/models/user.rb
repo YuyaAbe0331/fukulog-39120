fbd3784ee88e56f32e9e62b4b2b15c543d252cdc
YuyaAbe0331 <you8.kevin@gmail.com>
2023-07-20 19:11:18 +0900

    passwordのバリデーションの編集

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
   has_many :garments
   validates :nickname, presence: true
   validates :sex_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
-  validates :password,format: { with: /\A[a-z0-9]+\z/i }
+  # validates :password,format: { with: /\A[a-z0-9]+\z/i }
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :sex
   belongs_to :height
